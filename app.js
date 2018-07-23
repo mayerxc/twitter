7e93da8f8a843edea8bd9341a099631b6c1f4a1c
Chris Mayer <mayerxc@gmail.com>
2018-07-22 23:10:29 -0400

    added in some logic to get the twitter embed

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let client = new twitter({
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token_key: process.env.ACCESS_TOKEN_KEY,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
-  });
+});
 
 //Set up your search parameters
 let params = {
@@ -22,21 +22,41 @@ let params = {
 }
 
 let tweeter;
-client.get('search/tweets', params, function(error, tweets, response) {
-    if(error) throw error;
-    console.log(tweets.statuses[0].text);  // The favorites.
-    // console.log(response);  // Raw response object.
-    tweeter = tweets
-});
+// client.get('search/tweets', params, function(error, tweets, response) {
+//     if(error) throw error;
+//     console.log(tweets.statuses[0].text);  // The favorites.
+//     // console.log(response);  // Raw response object.
+//     tweeter = tweets
+// });
 
 
 
 //install and set up express
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => res.json(tweeter));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.use(cors());
 
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/client/index.html")));
+
+app.get('/query/:query', (req, res) => {
+    client.get('search/tweets', {
+            q: req.params.query,
+            count: 10,
+            result_type: 'recent',
+            lang: 'en'
+        },
+        function (error, tweets, response) {
+            if (error) throw error;
+            console.log(tweets.statuses[0].text); // The favorites.
+            // console.log(response);  // Raw response object.
+            tweeter = tweets
+        }
+    );
+    res.json(tweeter);
+});
+
+app.listen(3000, () => console.log('App listening on port 3000!'));
