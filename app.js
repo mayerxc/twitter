b11b5afd91263b2710ae8472157f2bd7d28708a3
Chris Mayer <mayerxc@gmail.com>
2018-07-29 23:55:44 -0400

    Add more CORS options

--- app.js
+++ app.js
@@ -43,6 +43,13 @@ const bodyParser = require('body-parser');
 
 //use cors
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
